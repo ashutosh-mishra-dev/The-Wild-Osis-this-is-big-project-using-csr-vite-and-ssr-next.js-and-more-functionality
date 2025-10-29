e6b56e42083e1f7612ce29536746cdbc286bb8d0
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-10-29 17:48:12 +0530

    Section 27 : React Query : Managing Remote State : fetching cabin data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,30 +1,18 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  useEffect(function () {
-    //ye promise ka throw data ko la rhe h
-    getCabins().then((data) => console.log(data));
-
-    //-------- async await ke throw data la rhe h  ----------------------
-    // async function fetchCabins() {
-    //   const data = await getCabins();
-    //   console.log(data);
-    // }
-    // fetchCabins();
-    //-----------------------------------------------------------
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      {/* <img
-        src="https://sarzztxidxgnsbagvswl.supabase.co/storage/v1/object/public/cabin-images/cabin-005.jpg"
-        alt="cabin"
-      /> */}
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
